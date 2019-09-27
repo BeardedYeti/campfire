41222d51559a05c7272174325944fe04f5464bc9
Colin Smith <cmsgoboston41@gmail.com>
2019-09-27 09:08:51 -0400

    Base scaffold

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: ["plugin:vue/essential", "@vue/prettier"],
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'graphql/template-strings': [
+      'error',
+      {
+        env: 'literal',
+        projectName: 'app',
+        schemaJsonFilepath: 'node_modules/.temp/graphql/schema.json'
+      }
+    ]
+  },
+
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)"],
+      env: {
+        jest: true
+      }
+    }
+  ],
+
+  plugins: [
+    'graphql'
+  ]
+};
